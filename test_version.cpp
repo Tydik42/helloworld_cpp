5362b5ef4529b05a774b7e4d2daad25b96861f6d
Tydik <tgaraev653@gmail.com>
2024-02-24 11:00:13 +0300

    yml and test fix

--- test_version.cpp
+++ test_version.cpp
@@ -3,9 +3,11 @@
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE_SUITE(test_version)
+BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
         BOOST_CHECK(version() > 0);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
